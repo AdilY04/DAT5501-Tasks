e3a46265f44c45ecac14056775ba6e5b8efb11ac
AdilY04 <adil.y04@outlook.com>
2025-11-21 08:07:55 +0000

    week8 iteration

---
+++ Week_8/dp_sorting.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import datetime as dt
+import time as ti
+
+df = pd.read_csv('Week_8/historical_data_GOOG2.csv')
+#print(df.head())
+#we see from the head that we will need to sort the formatting - will need to utilise datetime format + remove any $ signs + ensure float
+df['Date'] = pd.to_datetime(df['Date'], format='%m/%d/%Y')
+df['Close/Last'] = df['Close/Last'].str.replace('$','').astype(float)
+df['Open'] = df['Open'].str.replace('$','').astype(float)
+df['High'] = df['High'].str.replace('$','').astype(float)
+df['Low'] = df['Low'].str.replace('$','').astype(float)
+
+#assuming daily change in price is high-low
+df['Daily Change'] = df['Close/Last'].diff()
+print(df.head())
