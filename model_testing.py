c38a47a3cbe5c369fca88f4d0fb459e0a0842303
AdilY04 <adil.y04@outlook.com>
2025-11-29 10:42:34 +0000

    model fit week 9 fix

--- model_testing.py
+++ model_testing.py
@@ -15,7 +15,6 @@ Which model is the best?
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import re
 
 #cleaning the data - it has all different countries, i just want gdp of uk from 1922-2012 according to above instruction
 df = pd.read_csv('gdp-per-capita-maddison-project-database.csv', sep=',')
@@ -32,3 +31,26 @@ for i in range(1,10):
     p = np.poly1d(coeffs)
     all_coeffs[i] = coeffs
     all_fits[i] = p
+
+future_years = np.arange(2013,2023)
+
+all_forecasts = {}
+for i in range(1,10):
+    p = all_fits[i]
+    y_pred = p(future_years)
+    all_forecasts[i] = y_pred
+
+actual_future_values = df_uk['GDP per capita'][(df_uk['Year']>=2013)&(df_uk['Year']<=2022)].values
+
+#just have to turn forecasted values into np array so they can be plotted together
+forecast_arrays = np.array(list(all_forecasts.values()))
+
+#did use some AI assistance from here.
+x_future = future_years  
+plt.plot(x_future, actual_future_values, label="Actual", linewidth=3)
+
+for degree, forecast in all_forecasts.items():
+    plt.plot(x_future, forecast, label=f"Degree {degree}", alpha=0.5)
+
+plt.legend(loc='lower left')
+plt.show()
